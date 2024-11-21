e59cca89c40ab199e25fc961645d7474ef88db90
NGOCDG280\ngocdg <pnd280@gmail.com>
2024-11-21 08:11:31 +0700

    refactor: update prettier css config

--- src/features/options-page/dashboard/pages/plugins/hooks/useFilteredPlugins.ts
+++ src/features/options-page/dashboard/pages/plugins/hooks/useFilteredPlugins.ts
@@ -31,16 +31,20 @@ export function useFilteredPlugins({
           .toLowerCase()
           .includes(searchTerm.toLowerCase());
         const hasTags = plugin.tags !== undefined && plugin.tags.length > 0;
-        
+
         const matchesTags =
           selectedTags.length === 0 ||
           (hasTags && selectedTags.every((tag) => plugin.tags!.includes(tag)));
-        
-        const hasExcludedTags =
-          hasTags &&
-          excludeTags.some((tag) => plugin.tags!.includes(tag));
 
-        return matchesSearch && matchesTags && !hasExcludedTags && !isHiddenFromDashboard;
+        const hasExcludedTags =
+          hasTags && excludeTags.some((tag) => plugin.tags!.includes(tag));
+
+        return (
+          matchesSearch &&
+          matchesTags &&
+          !hasExcludedTags &&
+          !isHiddenFromDashboard
+        );
       })
       .map((plugin) => plugin.id);
   }, [searchTerm, selectedTags, excludeTags, featureFlags, userGroup]);

1a331432b32408afcd6e51d7f153b5a4a9d58781
Pham Ngoc Duong <pnd280@gmail.com>
2024-12-28 17:52:15 +0700

    fix(focus-selector): height overflow panel

--- src/features/plugins/query-box/focus-selector/components/DesktopSelectContent.tsx
+++ src/features/plugins/query-box/focus-selector/components/DesktopSelectContent.tsx
@@ -3,12 +3,12 @@ import { FOCUS_MODES } from "@/data/plugins/focus-selector/focus-modes";
 
 export function DesktopSelectContent() {
   return (
-    <SelectContent>
+    <SelectContent className="tw-max-h-[45dvh]">
       {FOCUS_MODES.map((mode) => (
         <SelectItem
           key={mode.code}
           item={mode.code}
-          className="tw-flex tw-flex-col tw-items-start tw-gap-2"
+          className="tw-flex tw-flex-col tw-items-start tw-gap-1"
         >
           <div className="tw-flex tw-items-center tw-gap-2">
             <mode.Icon className="tw-size-4" />
